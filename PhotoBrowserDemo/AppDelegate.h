2da5e6472988d9245d96b2b25336bb9b2ea2918e
948769029 <apple@appledeMacBook-Pro.local>
2017-03-03 09:45:46 +0800

    Initial Commit

---
+++ PhotoBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoBrowserDemo
+//
+//  Created by apple on 2017/3/3.
+//  Copyright © 2017年 张洪健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoBrowserDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoBrowserDemo
+//
+//  Created by apple on 2017/3/3.
+//  Copyright © 2017年 张洪健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
